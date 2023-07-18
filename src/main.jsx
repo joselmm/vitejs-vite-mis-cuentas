74ef109b534ad975e0e774266b65df8b7293f5a5
joselmm <95531879+joselmm@users.noreply.github.com>
2023-07-18 06:32:48 -0500

    2 files modified, 1 added, 2 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import PaymentFormReducer from './features/paymentForm';
+import PaymentsReducer from './features/payments';
+import PlatformNamesReducer from './features/platformNames';
+import PlatformsReducer from './features/platforms';
+import ClientsReducer from './features/clients';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import App from './App';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//configuring store
+const store = configureStore({
+  reducer: {
+    payments: PaymentsReducer,
+    //paymentForm: PaymentFormReducer,
+    clients: ClientsReducer,
+    platforms: PlatformsReducer,
+    platformNames: PlatformNamesReducer,
+  },
+});
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+  rootElement
+);
